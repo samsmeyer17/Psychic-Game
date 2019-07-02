50a97aa47aedfd7ff9d7de2609d919192660507a
Samuel Meyer <sam.smeyer17@gmail.com>
2019-07-01 22:34:42 -0700

    added javascript to allow for counting wins and losses and generating random numbers and userGuesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,27 @@
 src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"
 
-var wins = 0;
-var losses = 0;
-var randomNum = Math.floor(Math.random * 100) + 1;
-var userGuess = prompt("Pick A Number Between 1 and 100!");
-
-
-$("#main-button").on("click", function(){
-    Math.floor(Math.random * 100) + 1;
-    prompt("Pick A Number Between 1 and 100!");
-    if (userGuess === randomNum) {
-        alert("You Guessed Correctly!");
-        wins++;
-    }
-
-    else {
-        alert("You Guessed Incorectly!");
-        losses++;
-    }
+$(document).ready(function () {
     
-})
+    var wins = 0;
+    var losses = 0;
+    
+    
+
+
+    $("#main-button").on("click", function(){
+        var randomNum = Math.floor(Math.random * 100) + 1;
+        var userGuess = prompt("Pick A Number Between 1 and 100!");
+        prompt("Pick A Number Between 1 and 100!");
+        if (userGuess === randomNum) {
+            alert("You Guessed Correctly! You Have Won " + wins + " times!");
+            wins++;
+
+        }
+
+        else {
+            alert("You Guessed Incorectly! You Have Lost " + losses + " Times!");
+            losses++;
+        }
+    })
+
+});
